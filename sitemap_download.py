94e9a00e363580523a6f9951273432bbad163518
Martin Janeček <admin@ikw.cz>
2024-01-30 11:15:31 +0100

    feat(sitemap download): output example and result info

--- sitemap_download.py
+++ sitemap_download.py
@@ -1,12 +1,19 @@
 from usp.tree import sitemap_tree_for_homepage
 import sys
 
+# load and validate arguments
+url = sys.argv[1] if len(sys.argv) > 1 else None
+
+if not url or url.startswith('http'):
+    print('ERR: Provide a valid url')
+    print('Example: python sitemap_download.py https://www.example.com')
+    sys.exit(1)
+
+
 # download sitemap and save to file
-
-url = sys.argv[1] if len(sys.argv) > 1 else 'https://exampl.com/'
-
 tree = sitemap_tree_for_homepage(url)
+urls = [page.url for page in tree.all_pages()]
 
 with open('sitemap.txt', 'w') as f:
-    for sitemap in tree.all_pages():
-        f.write(sitemap.url + '\n')
+    f.write('\n'.join(urls))
+    print(f'File sitemap.txt created with {len(urls)} urls')
