a065c6b66f7404c01bbaeeb44da6e09bf6068320
Martin Janeček <admin@ikw.cz>
2024-01-30 11:16:34 +0100

    feat(locust): provide defaults for wait_time and host

--- locustfile.py
+++ locustfile.py
@@ -1,13 +1,21 @@
-import math
-from pathlib import Path
-from locust import LoadTestShape, between, task
-from locust.contrib.fasthttp import FastHttpUser
 import random
+from pathlib import Path
+
+from locust import task, constant_throughput
+from locust.contrib.fasthttp import FastHttpUser
 
 testing_urls = Path('sitemap.txt').read_text().splitlines()
 
 
 class WebUser(FastHttpUser):
+    host = testing_urls[0]
+
+    """
+    0.2 = task runs at most 0.2 times per second, i.e. once every 5 seconds on average
+    
+    @see https://docs.locust.io/en/stable/writing-a-locustfile.html#wait-time-attribute
+    """
+    wait_time = constant_throughput(0.2)
 
     @task
     def load_test(self):
